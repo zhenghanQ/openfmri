4416633cdceef95e92de7c4346efb38c11b87a2b
Kevin Sitek <ksitek@mit.edu>
2015-03-23 11:09:07 -0400

    added -c dcm2nii

--- dicom2nifti/dcmconv.sh
+++ dicom2nifti/dcmconv.sh
@@ -4,10 +4,11 @@ for sub in voice_984 voice_983 voice_982 voice_981 voice_980 voice_979 voice_979
 #for subdir in /mindhive/xnat/dicom_storage/voice/voice_*; do
 	#sub=$(basename $subdir)
 	rm dcmconv_${sub}.sh
+	rm -rf /om/project/voice/RawData/voice/$sub/session001/info
 	echo "#!/bin/bash
 	python dicomconvert2.py \
 		-d /mindhive/xnat/dicom_storage/voice/%s/*/* \
-		-s $sub \
+		-s $sub \ -c dcm2nii \
 		-f heuristic_example.py \
 		-o /om/project/voice/RawData/voice/" >> dcmconv_${sub}.sh
 
