36072be793f7f9f379036169f70d46ca9e0103f3
Kevin Sitek <ksitek@mit.edu>
2015-03-23 10:57:46 -0400

    corrects subdir issue

--- dicom2nifti/dcmconv.sh
+++ dicom2nifti/dcmconv.sh
@@ -2,13 +2,13 @@
 
 for sub in voice_984 voice_983 voice_982 voice_981 voice_980 voice_979 voice_979b; do
 #for subdir in /mindhive/xnat/dicom_storage/voice/voice_*; do
-	sub=$(basename $subdir)
+	#sub=$(basename $subdir)
 	rm dcmconv_${sub}.sh
 	echo "#!/bin/bash
 	python dicomconvert2.py \
 		-d /mindhive/xnat/dicom_storage/voice/%s/*/* \
 		-s $sub \
-		-f heuristic_init.py \
+		-f heuristic_example.py \
 		-o /om/project/voice/RawData/voice/" >> dcmconv_${sub}.sh
 
 	sbatch --mem=20G dcmconv_${sub}.sh
