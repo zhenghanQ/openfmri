4baf4853f1b5dc0bbfacea272a039822fac06f24
Kevin Sitek <ksitek@mit.edu>
2015-03-23 10:30:18 -0400

    updated for new structure, still not working

--- dicom2nifti/dcmconv.sh
+++ dicom2nifti/dcmconv.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-python dicomconvert2.py \
-	-d /mindhive/xnat/dicom_storage/voice/%s/*/* \
-	-s voice_999b voice_999_20150304 voice_986 voice_899 \
-	-f heuristic.py -o /om/project/voice/mri/openfmri
+for sub in voice_984 voice_983 voice_982 voice_981 voice_980 voice_979 voice_979b; do
+#for subdir in /mindhive/xnat/dicom_storage/voice/voice_*; do
+	sub=$(basename $subdir)
+	rm dcmconv_${sub}.sh
+	echo "#!/bin/bash
+	python dicomconvert2.py \
+		-d /mindhive/xnat/dicom_storage/voice/%s/*/* \
+		-s $sub \
+		-f heuristic_init.py \
+		-o /om/project/voice/RawData/voice/" >> dcmconv_${sub}.sh
+
+	sbatch --mem=20G dcmconv_${sub}.sh
+done
