0aee35d1fddfdd3fd64bbbf07d604d84d8a6d67a
Kevin Sitek <ksitek@mit.edu>
2015-09-03 13:02:27 -0400

    run subjects through ppi

---
+++ subject_level/ppi_single_sub.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+openfmri_dir=/om/project/voice/processedData/openfmri/
+out_dir=/om/project/voice/processedData/l1analysis/ppi/
+session_id=session001
+
+task=5
+for s in ${openfmri_dir}voice975; do
+	sub=${s: -8}
+	echo $sub
+	SUBJECTS_DIR=/om/project/voice/processedData/fsdata/$sub/
+	python fmri_ants_openfmri_sparse_ppi.py \
+	-d $openfmri_dir \
+	-m 001 \
+	-x $sub \
+	--sd /om/project/voice/processedData/fsdata/$sub/ \
+	-o $out_dir \
+	-w /om/scratch/Thu/ksitek/ppi/task00${task}/$sub \
+	-t $task \
+	--session_id $session_id \
+	--plugin "SLURM"
+done
