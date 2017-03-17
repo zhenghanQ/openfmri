3a7622873f909245030d4d2eb95fc4a605c7ff9f
ZhenghanQ <zhenghan.qi@gmail.com>
2017-03-17 18:03:32 -0400

    use singularity command to run

--- convert/dcm2nii_bids.sh
+++ convert/dcm2nii_bids.sh
@@ -6,13 +6,13 @@ do
 subj=$(basename $subj)
 mkdir ${temp_dir}/${subj}_dicom
 cd ${temp_dir}/${subj}_dicom
-echo '#!bin/bash' > dicom_run.sh
-echo "heudiconv -d /mindhive/xnat/dicom_storage/CASL/%s/dicom/*.dcm -o /om/user/zqi/projects/CASL/Results/Imaging/openfmri -f /om/user/zqi/projects/CASL/Analysis/bids/openfmri/convert/heuristic_CASL_bids.py -c dcm2niix -q om_all_nodes -s ${subj} -b" >> dicom_run.sh
+echo '#!/bin/bash' > dicom_run.sh
+echo "singularity exec -B /mindhive/xnat/dicom_storage/CASL/:/dicomdir -B /om/user/zqi/projects/CASL/Results/Imaging/openfmri/:/output -B /om/user/zqi/projects/CASL/Analysis/bids/openfmri/convert/:/mnt -c /storage/gablab001/data/singularity-images/heudiconv/nipy/heudiconv heudiconv -d /dicomdir/%s/dicom/*.dcm -c dcm2niix -o /output -f /mnt/heuristic_CASL_bids.py -s ${subj} -b" >> dicom_run.sh
 done
 for subj in `ls -d /mindhive/xnat/dicom_storage/CASL/CASL*`
 do
 subj=$(basename $subj)
 cd ${temp_dir}/${subj}_dicom
-bash dicom_run.sh
+sbatch -t 2:00:00 --mem=20GB -c2 dicom_run.sh
 done
 
